7a3bf81f6621e0759061326d2f12c5f4ba57c230
Matthew Reddin <m.reddin@me.com>
2022-07-27 18:03:15 +0100

    Updated TMDB key

--- Top Media/AppConstants.swift
+++ Top Media/AppConstants.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct AppConstants {
     // As a portfolio app API keys are provided for convenience and are not connected to a paid account
-    static var tvdmAPIKey = "15f6434646162c8b0fdd907f55f27e3b"
+    static var tvdmAPIKey = "c16bbcce6e8dabf8039ef27ef24684fa"
     static let youtubeAPiKey = "AIzaSyCBpzAJgdfhBMdLRAQJDfyASItLm3FLuWg"
 }
